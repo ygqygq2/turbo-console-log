96f8faa48c9fa2ec0975e56277a2689a12143922
ygqygq2 <ygqygq2@qq.com>
2024-01-31 00:34:38 +0800

    test: bdd 可以运行
    Co-authored-by: ygqygq2 <ygqygq2@qq.com>

--- src/test/unit/index.spec.ts
+++ src/test/unit/index.spec.ts
@@ -1,21 +1,33 @@
-import { add, subtract } from '@/mocha-test';
-import { expect } from 'chai';
+// import { add, subtract } from '@/mocha-test';
+// import { expect } from 'chai';
 
-// describe和it函数用于组织测试套件和测试用例
-describe('Calculator', () => {
-  // 编写add方法的测试用例
-  describe('add', () => {
-    it('should return the sum of two numbers', () => {
-      const result = add(2, 3);
-      expect(result).to.equal(5);
-    });
+// // describe和it函数用于组织测试套件和测试用例
+// describe('Calculator', () => {
+//   // 编写add方法的测试用例
+//   describe('add', () => {
+//     it('should return the sum of two numbers', () => {
+//       const result = add(2, 3);
+//       expect(result).to.equal(5);
+//     });
+//   });
+
+//   // 编写subtract方法的测试用例
+//   describe('subtract', () => {
+//     it('should return the difference between two numbers', () => {
+//       const result = subtract(5, 3);
+//       expect(result).to.equal(2);
+//     });
+//   });
+// });
+
+import { mochaTest } from '@/mocha-test';
+import { equal } from 'assert';
+
+describe('Typescript usage suite', () => {
+  it('should be able to execute a test', () => {
+    equal(true, true);
   });
-
-  // 编写subtract方法的测试用例
-  describe('subtract', () => {
-    it('should return the difference between two numbers', () => {
-      const result = subtract(5, 3);
-      expect(result).to.equal(2);
-    });
+  it('should return expected string', () => {
+    equal(mochaTest('incoming'), 'incoming-static');
   });
 });
