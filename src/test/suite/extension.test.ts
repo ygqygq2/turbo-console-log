71cbc5a995628775b4d33cf0be03b5116bf4f9cd
ygqygq2 <ygqygq2@qq.com>
2024-02-09 23:54:14 +0800

    test: 尝试集成测试
    Signed-off-by: ygqygq2 <ygqygq2@qq.com>

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,29 +1,53 @@
-import * as assert from 'assert';
-import { after } from 'mocha';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
+import { displayLogMessageCommand } from '@/commands/displayLogMessage';
+import { ExtensionProperties } from '@/typings/extension/types';
+import { test, suite, expect } from 'vitest';
 import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
 
-suite('Extension Test Suite1', () => {
+suite.todo('Extension Test Suite', () => {
+  let mockExtensionProperties: ExtensionProperties;
+  let testEditor: vscode.TextEditor;
   suiteTeardown(() => {
     vscode.window.showInformationMessage('All tests done!');
   });
 
-  test('Sample test1', () => {
-    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-  });
-});
+  beforeEach(async () => {
+    mockExtensionProperties = {
+      wrapLogMessage: false,
+      logMessagePrefix: '🚀',
+      logMessageSuffix: ':',
+      addSemicolonInTheEnd: true,
+      insertEmptyLineBeforeLogMessage: false,
+      insertEmptyLineAfterLogMessage: false,
+      quote: '"',
+      delimiterInsideMessage: '~',
+      includeFileNameAndLineNum: true,
+      logFunction: {},
+    };
 
-suite('Extension Test Suite2', () => {
-  after(() => {
-    vscode.window.showInformationMessage('All tests done!');
+    testEditor = await vscode.workspace.openTextDocument({
+      content: '// sample content',
+    });
+    testEditor = await vscode.window.showTextDocument(testEditor);
   });
 
-  test('Sample test2', () => {
-    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+  afterEach(async () => {
+    await testEditor.dispose();
   });
+
+  test.todo('Display Log Message', async () => {
+    await displayLogMessageCommand().handler(mockExtensionProperties);
+
+    expect(vscode.window.activeTextEditor?.edit).toHaveBeenCalled();
+
+    expect(mockEditor.edit).toHaveBeenCalledWith(expect.any(Function));
+    expect(mockEditBuilder.insert).toHaveBeenCalledWith(expect.any(Position), expect.any(String));
+  });
+
+  test.todo('Comment All Log Message', () => {});
+
+  test.todo('Uncomment All Log Message', () => {});
+
+  test.todo('Delete All Log Message', () => {});
+
+  test.todo('Update Line Number For All Log Messages', async () => {});
 });
